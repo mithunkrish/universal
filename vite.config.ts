a784c293e303b62a576d66cfcb3aa19aa3d22a5b
mithunkrish <69386691+mithunkrish@users.noreply.github.com>
2025-11-02 21:52:14 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,19 +5,23 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
+        // 👇️ यहाँ 'base' प्रोपर्टी थपिएको छ
+        base: '/universal/', 
+        // 👆️ GitHub Pages (https://mithunkrish.github.io/universal/) मा 404 त्रुटि समाधान गर्नको लागि
+        
+        server: {
+            port: 3000,
+            host: '0.0.0.0',
+        },
+        plugins: [react()],
+        define: {
+            'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+            'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+        },
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, '.'),
+            }
         }
-      }
     };
 });
